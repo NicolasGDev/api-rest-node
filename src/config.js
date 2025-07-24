d32c595089019687e26bff96fdd7fb47c3a3a06b
Nicolas Gutierrez <gutierreznicolas2004@gmail.com>
2025-07-23 19:41:45 -0500

    sexto commit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
+// config.js
 if (process.env.NODE_ENV !== 'production') {
-  import('dotenv').then(({ config }) => config());
+  const dotenv = await import('dotenv');
+  dotenv.config();
 }
 
+export const PORT = process.env.PORT;
 export const DB_HOST = process.env.DB_HOST;
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
